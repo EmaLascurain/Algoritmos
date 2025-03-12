bc3bbf8c84bd26e8279e87a7d3ed2a810a4738ab
Joaquin Machado <joaquin.machado@ingenieria.uner.edu.ar>
2025-03-12 12:05:20 -0300

    arego funcion

--- TrabajoPractico_1/proyecto_1/modules/modulo1.py
+++ TrabajoPractico_1/proyecto_1/modules/modulo1.py
@@ -1,2 +1,8 @@
 # módulo para organizar funciones o clases utilizadas en nuestro proyecto
 # Crear tantos módulos como sea necesario para organizar el código
+
+def funcion1():
+    print ("Taylor Swift")
+    
+def funcion2():
+    print("es la mejor")
